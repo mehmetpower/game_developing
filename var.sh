dea17e2582168dc6402b28b4f6f63ce5f3a62424
mehmetpower <mehmet.avci@powercoders.org>
2023-09-22 19:53:22 +0200

    pass variables to var.sh file & colorize the game text

---
+++ var.sh
@@ -0,0 +1,43 @@
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+PURPLE='\033[0;35m'
+RESET='\033[0m'
+
+a=0
+n=0
+
+questions=("Q1" "Q2" "Q3" "Q4" "Q5" "Q6" "Q7" "Q8" "Q9" "Q10")
+answers=("A1" "A2" "A3" "A4" "A5" "A6" "A7" "A8" "A9" "A10")
+
+
+
+Q1="If two's company, and three's a crowd, what are four and five? : "
+A1='9'
+
+Q2="What three numbers, none of which is zero, give the same result whether they're added or multiplied? : "
+A2='1 2 3'
+
+Q3="Mary has four daughters, and each of her daughters has a brother. How many children does Mary have? : "
+A3='5'
+
+Q4="Three doctors said that Bill was their brother. Bill says he has no brothers. How many brothers does Bill actually have? : "
+A4='0'
+
+Q5="A little girl goes to the store and buys one dozen eggs. As she is going home, all but three break. How many eggs are left unbrocken? : "
+A5='3'
+
+Q6="If there are three apples and you take away two, how many apples do you have? : "
+A6='2'
+
+Q7="I am a number with a couple of friends, quarter a dozen, and youll find me again. What am I? : "
+A7='3'
+
+Q8="How many 9s are there between 1 and 100? : "
+A8='20'
+
+Q9="What is half of two plus two? : "
+A9='2'
+
+Q10="An apple is 40 cents, a banana is 60 cents and a grapefruit is 80 cents. How much is a pear? : "
+A10='40'
