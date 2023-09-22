0b780fce72825f6356948dc3599e102b333dbc73
Ababaikeli Kahaerzhati <ababaikeli.kahaerzhati@powercoders.org>
2023-09-22 15:02:25 +0200

    2 questions for test

---
+++ qz_game_file.sh
@@ -0,0 +1,10 @@
+read -p "Enter Your Name: " fname
+echo "Player name is: $fname"
+
+echo "First riddle: If two's company, and three's a crowd, what are four and five ?"
+read -p "Enter your answer: " fanswer1
+
+
+echo "Second riddle: What three numbers, none of which is zero, give the same result whether they're added or multiplied ?"
+read -p "Enter your answer: " fanswer2
+
