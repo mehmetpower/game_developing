b03d59b94abf7183a55c8a4d66d65874d75fd137
Ababaikeli Kahaerzhati <ababaikeli.kahaerzhati@powercoders.org>
2023-09-22 15:22:37 +0200

    all riddles have been uploaded

--- qz_game_file.sh
+++ qz_game_file.sh
@@ -8,3 +8,28 @@ read -p "Enter your answer: " fanswer1
 echo "Second riddle: What three numbers, none of which is zero, give the same result whether they're added or multiplied ?"
 read -p "Enter your answer: " fanswer2
 
+echo "Third riddle: Mary has four daughters, and each of her daughters has a brother. How many children does Mary have ?"
+read -p "Enter your answer: " fanswer3
+
+echo "Fourth riddle: Three doctors said that Bill was their brother. Bill says he has no brothers. How many brothers does Bill actually have ?"
+read -p "Enter your answer: " fanswer4
+
+echo "Fifth riddle: A little girl goes to the store and buys one dozen eggs. As she is going home, all but three break. How many eggs are left unbrocken ?"
+read -p "Enter your answer: " fanswer5
+
+echo "Sixth riddle: If there are three apples and you take away two, how many apples do you have ?"
+read -p "Enter your answer: " fanswer6
+
+echo "Seventh riddle: I am a number with a couple of friends, quarter a dozen, and you'll find me again.
+What am I ?"
+read -p "Enter your answer: " fanswer7
+
+echo "Eighth riddle: How many 9's are there between 1 and 100 ?"
+read -p "Enter your answer: " fanswer8
+
+echo "Nineth riddle: What is half of two plus two ?"
+read -p "Enter your answer: " fanswer9
+
+echo "Tenth riddle: An apple is 40 cents, a banana is 60 cents and a grapefruit is 80 cents. How much is a pear ?"
+read -p "Enter your answer: " fanswer10
+
